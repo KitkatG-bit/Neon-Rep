d9a4c2a314aca2cebb2f96b11fa51aac767866c4
Kitkat <bellybelinha30@gmail.com>
2025-01-30 20:54:25 -0300

    Update daily.js

--- daily.js
+++ daily.js
@@ -2,7 +2,7 @@ module.exports = [{
 name: "daily",
 aliases: ["diária", "diário"],
 code: `
-Você recebeu $random[200;1000]
-$setUserVar[neon;$sum[$getUserVar[neon;$authorID];$random[200;1000]]]]
-Bastões Luminosos hehehhe    `
+Você recebeu $random[200;1000] Bastões Luminosos hehehhe 💰
+$addButton[1;$random[200;1000;primary;$random[200;1000;true]
+$setUserVar[neon;$sum[$getUserVar[neon;$authorID];$random[200;1000]]]]`
 }]  
